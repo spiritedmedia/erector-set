cc171e552de9acd2a466996b690f3746d655ee97
Russell Heimlich <rheimlich@Tomlinsons-MacBook-Pro.local>
2016-05-10 17:09:36 -0400

    Clone our /wp-content/ repo into /public/ first
    EasyEngine won't overwrite it when it installs WordPress and once the box is provisioned we can circle back and run Composer, NPM, and Bower and all that fun stuff.

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Install the hostupdater plugin
 # vagrant plugin install vagrant-hostsupdater
 
+# Clone our /wp-content/ repo into /public/
+git clone git@github.com:spiritedmedia/pedestal-beta.git public/
+
 # Build the box
 vagrant up
 
