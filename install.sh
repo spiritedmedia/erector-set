43fda7c29f7095e0aa838307a36462f7cd8245f6
Russell Heimlich <kingkool68@gmail.com>
2016-05-17 17:52:23 -0400

    Lots of new things
    - Check to make sure certain commands exist on the host machine so we can build the theme.
    
    - Make our utility helper scripts executable.
    
    - Ask if we can delete the entire `/public` directory before cloning the repo in to place. Otherwise we skip this step and continue provisioning the box.
    
    - Make sure a debug.log file exists in `/public/wp-content/` for when `define( 'WP_DEBUG_LOG' )` is set to true.
    
    - Add steps to actually build Pedestal.
    
    - Display how long it took to run the install script.

--- install.sh
+++ install.sh
@@ -1,23 +1,75 @@
 #! /bin/bash
 
+function command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+# By storing the date now, we can calculate the duration of provisioning at the
+# end of this script.
+start_seconds="$(date +%s)"
+
+if ! command_exists npm ; then
+    echo "NPM isn't installed. You need that to build the theme."
+fi
+if ! command_exists bower ; then
+    echo "Bower isn't installed. You need that to build the theme."
+fi
+if ! command_exists bundle ; then
+    echo "Bundle isn't installed. You need that to build the theme."
+fi
+if ! command_exists composer ; then
+    echo "Composer isn't installed. You need that to build the theme."
+fi
+if ! command_exists grunt ; then
+    echo "Grunt isn't installed. You need that to build the theme."
+fi
+
+# Make our utility helper scripts executeable
+chmod +x utilities/error-logging.sh
+chmod +x utilities/get-db-info.sh
+
 # Check if the /public directory is empty or not
 if [ "$(ls -A public/)" ]; then
     echo "The /public directory is not empty."
     while true; do
-    read -p "Delete everything in /public? (y/n): " yn
+    read -p "Delete everything in /public and start over? (y/n): " yn
     case $yn in
-        [Yy]* ) rm -rf public/; break;;
-        [Nn]* ) echo "Aborting install"; exit;; # TODO: Don't exit but instead just skip the step of cloning the repo. 
-        * ) echo "Please answer yes or no.";;
+        [Yy]* )
+            rm -rf public/
+            # Clone our /wp-content/ repo into /public/
+            git clone --recursive git@github.com:spiritedmedia/pedestal-beta.git public/
+            break
+            ;;
+        [Nn]* )
+            echo "Skipping...";
+            break
+            ;;
+        * ) echo
+            "Please answer yes or no."
+            ;;
     esac
 done
 fi
-
-# Clone our /wp-content/ repo into /public/
-git clone --recursive git@github.com:spiritedmedia/pedestal-beta.git public/
 
 # Update the box
 vagrant box update
 
 # Build the box
 vagrant up
+
+# Add a debug.log file for when define('debuglog') is set to true
+cd public/wp-content/
+touch debug.log
+
+# Go to pedestal directory and build the theme
+cd themes/pedestal/
+npm install
+bower install
+bundle install
+composer update -o
+grunt build
+
+# And it's done
+end_seconds="$(date +%s)"
+echo "-----------------------------"
+echo "Installation complete in "$((${end_seconds} - ${start_seconds}))" seconds"
