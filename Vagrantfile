9be9090b33bd81c0c2446046cb4b59d48f34c9e3
Chris Montgomery <chris@montchr.io>
2018-08-08 16:58:29 -0500

    Use the conditionals we have already

--- Vagrantfile
+++ Vagrantfile
@@ -128,7 +128,6 @@ Vagrant.configure("2") do |config|
     config.vm.provision "file", source: "config/nginx-configs", destination: "~/nginx-configs"
     config.vm.provision "file", source: "config/php-configs", destination: "~/php-configs"
     config.vm.provision "shell", path: "config/easyengine.sh"
-    config.vm.provision "shell", path: "config/update-configs.sh"
     # config.vm.provision "shell", path: "config/build-tools.sh"
     # config.vm.synced_folder "logs/", "/var/log/", owner: "root", group: "root"
 
--- config/easyengine.sh
+++ config/easyengine.sh
@@ -11,7 +11,39 @@ function divider() {
     fi
 }
 
-if ! command_exists ee ; then
+if command_exists ee ; then
+
+    divider "Moving nginx confs into place"
+    # Move nginx conf files into place for each mapped domain
+    cd /home/ubuntu/nginx-configs/
+    filenames=(*.dev)
+    sudo mv *.dev /etc/nginx/sites-available/
+    for filename in "${filenames[@]}"
+    do
+        sudo ln -s /etc/nginx/sites-available/$filename /etc/nginx/sites-enabled/$filename
+        echo "$filename symlinked";
+    done
+
+    divider "Moving PHP extension config files into place"
+    xdebug_ini_path=$(php --ini | grep xdebug | head -1)
+    mods_available_path=$(dirname $(readlink -f "${xdebug_ini_path//,}"))
+    cd /home/ubuntu/php-configs/
+    sudo mv *.ini "$mods_available_path"/
+
+    divider "Moving local-photon configs into place"
+    sudo mv local-photon.nginx.conf /var/www/spiritedmedia.dev/conf/nginx/local-photon.nginx.conf
+    sudo cp photon-config.php /var/www/spiritedmedia.dev/photon/config.php
+    sudo chown -R www-data: /var/www/spiritedmedia.dev/photon/
+
+    divider "Cleaning up"
+    cd ../
+    sudo rm -rf nginx-configs/
+    sudo rm -rf php-configs/
+
+    divider "All done. Restarting the stack."
+    sudo ee stack restart
+
+else
 
     divider "Installing Unattended Upgrades"
     sudo apt install unattended-upgrades
